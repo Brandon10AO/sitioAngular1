a483111a7bb1dda170e719860177beb0148e6af9
BrandonAguilarOrtega <aguuilarbrandon5@gmail.com>
2021-04-04 17:09:16 -0500

    Se realizaron los cambios para agregar los superheroes en lista dinamica

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from '../hero';
+
+import {HEROES} from '../mock.heroes';
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -7,19 +10,15 @@ import {Hero} from '../hero';
 })
 export class HeroesComponent implements OnInit {
 
-  hero: Hero= {
-  id:1,
-  name: 'Spiderman',
-  edad: 20,
-  Poder: 'Poderes Aracnidos',
-  Identidad: 'peter parker',
-  Universo: 'Marvel',
-
-  };
+  heroes = HEROES;
+  selectedHero?: Hero;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
 }
---
+++ src/app/mock.heroes.ts
@@ -0,0 +1,14 @@
+import {Hero}from "./hero";
+
+export const HEROES: Hero[] = [
+{id: 11, name: "Superman", edad: 30,Poder: "volar y fuerza",Identidad: "Clark Kent",Universo: "DC"},
+{id: 12, name: "Batman", edad: 30,Poder: "habilidad",Identidad: "Bruce Wayne",Universo: "DC"},
+{id: 13, name: "Hulk", edad: 40,Poder: "super fuerza",Identidad: "bruce banner",Universo: "marvel"},
+{id: 14, name: "ironman", edad: 25,Poder: "volar",Identidad: "tony stars",Universo: "marvel"},
+{id: 15, name: "Thor", edad: 1000,Poder: "rayos",Identidad: "thor",Universo: "marvel"},
+{id: 16, name: "Spiderman", edad: 20,Poder: "sentido aracnido",Identidad: "peter parker",Universo: "marvel"},
+{id: 17, name: "Black Widow", edad: 28,Poder: "aguilidad sobrehumana",Identidad: "Natalia Alianovna",Universo: "marvel"},
+{id: 18, name: "Flash", edad: 28,Poder: "velocidad",Identidad: "barry allin",Universo: "DC"},
+{id: 19, name: "Bruja Escarlata", edad: 36,Poder: "hechizos",Identidad: "Wanda",Universo: "marvel"},
+{id: 20, name: "Wonder Woman", edad: 30,Poder: "volar y fuerza",Identidad: " Diana Prince",Universo: "DC"}
+];
